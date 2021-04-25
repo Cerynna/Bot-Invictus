7c2b0df4a04583109c7e191e635589a7949f675c
Jeremy <jeremy@maans.eu>
2021-04-25 06:36:07 +0200

    CommitAtomic

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Invictus Bot
-[Link](http://https://discord.com/api/oauth2/authorize?client_id=835306218158817340&permissions=8&scope=bot "Link")
+[Link](https://discord.com/api/oauth2/authorize?client_id=835306218158817340&permissions=8&scope=bot "Link")
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 // const config = require("./config.json");
 const client = new Discord.Client();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const Users = require("./Users");
 const jobs = [
@@ -25,17 +27,33 @@ const jobs = [
   "Tailleur",
 ];
 
-// console.log(jobs);
-
 const prefix = "&";
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("&help pour m'invoquer");
+});
+
 client.on("message", async (message) => {
-  // console.log(message.guild.members.cache.get())
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
 
   const commandBody = message.content.slice(prefix.length);
   const args = commandBody.split(" ");
   const command = args.shift().toLowerCase();
+  if (command === "clear") {
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      await message.channel.messages.fetch().then(
+        function (list) {
+          message.channel.bulkDelete(list);
+        },
+        function (err) {
+          message.channel.send("ERROR: ERROR CLEARING CHANNEL.");
+        }
+      );
+
+      return;
+    }
+  }
   if (command === "job") {
     if (args.length === 0) {
       let fields = await Promise.all(
@@ -56,11 +74,12 @@ client.on("message", async (message) => {
       );
       const embed = new Discord.MessageEmbed()
         .setColor("#fbff00")
-        .setTitle(`Liste de tout les metiers du bot`)
+        .setTitle(`Liste de tout les metiers`)
         .addFields(fields)
         .setTimestamp();
 
       message.reply(embed);
+      message.delete();
       return;
     }
     if (args[0] === "-list") {
@@ -71,20 +90,51 @@ client.on("message", async (message) => {
           })
           .join("")}${"```"}`
       );
+      message.delete();
       return;
     }
+    if (args[0] === "-delete") {
+      if (!message.member.hasPermission("ADMINISTRATOR")) {
+        // message.channel.send(`Tu n'es pas Admin`);
+        return;
+      }
+      if (!args[1].match(/\d+/)) {
+        // message.channel.send(`Pas d'user`);
+        return;
+      }
+      const idUser = args[1].match(/\d+/)[0];
 
+      await Users.delete(idUser);
+
+      message.channel.send(`${args[1]} n'as plus de métiers`);
+      message.delete();
+      return;
+    }
     if (!args[0].match(/\d+/)) {
       if (
         !jobs
           .map((x) => x.toLocaleLowerCase())
           .includes(args[0].toLocaleLowerCase())
       ) {
-        message.reply(`HUUUMMM ${args[0]} n'ai pas un metier connu`);
+        message.channel
+          .send(`HUUUMMM ${args[0]} n'ai pas un metier connu`)
+          .then((msg) => {
+            setTimeout(() => {
+              msg.delete();
+            }, 5000);
+          });
+        message.delete();
         return;
       }
       if (isNaN(args[1])) {
-        message.reply(`HUUUMMM ${args[1]} n'ai pas un lvl`);
+        message.channel
+          .send(`HUUUMMM ${args[1]} n'ai pas un lvl`)
+          .then((msg) => {
+            setTimeout(() => {
+              msg.delete();
+            }, 5000);
+          });
+        message.delete();
         return;
       }
 
@@ -95,31 +145,58 @@ client.on("message", async (message) => {
         };
         job[args[0].toLocaleLowerCase()] = args[1];
         user = await Users.set(message.author.id, job);
-        message.channel.send(
-          `${job.name} est maintenant ${args[0]} lvl : ${args[1]}`
-        );
+        message.channel
+          .send(`${job.name} est maintenant ${args[0]} lvl : ${args[1]}`)
+          .then((msg) => {
+            setTimeout(() => {
+              msg.delete();
+            }, 5000);
+          });
+        message.delete();
         return;
       }
       user[args[0].toLocaleLowerCase()] = args[1];
 
       await Users.set(message.author.id, user);
 
-      message.channel.send(
-        `${user.name} est maintenant ${args[0]} lvl : ${args[1]}`
-      );
+      message.channel
+        .send(`${user.name} est maintenant ${args[0]} lvl : ${args[1]}`)
+        .then((msg) => {
+          setTimeout(() => {
+            msg.delete();
+          }, 5000);
+        });
+      message.delete();
       return;
     }
 
     if (!jobs.map((x) => x.toLocaleLowerCase()).includes(args[1])) {
-      message.reply(`HUUUMMM ${args[1]} n'ai pas un metier connu`);
+      message
+        .reply(`HUUUMMM ${args[1]} n'ai pas un metier connu`)
+        .then((msg) => {
+          setTimeout(() => {
+            msg.delete();
+          }, 5000);
+        });
+      message.delete();
       return;
     }
     if (isNaN(args[2])) {
-      message.reply(`HUUUMMM ${args[2]} n'ai pas un lvl`);
+      message.reply(`HUUUMMM ${args[2]} n'ai pas un lvl`).then((msg) => {
+        setTimeout(() => {
+          msg.delete();
+        }, 5000);
+      });
+      message.delete();
       return;
     }
 
     const idUser = args[0].match(/\d+/)[0];
+    // const memberPermissions = message.member.hasPermission('ADMINISTRATOR');
+    // if()
+    // console.log(memberPermissions);
+    if (!message.member.hasPermission("ADMINISTRATOR")) return;
+
     let user = await Users.get(idUser);
     if (!user) {
       let job = {
@@ -127,34 +204,53 @@ client.on("message", async (message) => {
       };
       job[args[1].toLocaleLowerCase()] = args[2];
       user = await Users.set(idUser, job);
-      message.channel.send(
-        `${job.name} est maintenant ${args[1]} lvl : ${args[2]}`
-      );
+      message.channel
+        .send(`${job.name} est maintenant ${args[1]} lvl : ${args[2]}`)
+        .then((msg) => {
+          setTimeout(() => {
+            msg.delete();
+          }, 5000);
+        });
       return;
     }
     user[args[1].toLocaleLowerCase()] = args[2];
 
     await Users.set(idUser, user);
 
-    message.channel.send(
-      `${user.name} est maintenant ${args[1]} lvl : ${args[2]}`
-    );
+    message.channel
+      .send(`${user.name} est maintenant ${args[1]} lvl : ${args[2]}`)
+      .then((msg) => {
+        setTimeout(() => {
+          msg.delete();
+        }, 5000);
+      });
+    message.delete();
     return;
   }
 
   if (command === "help") {
-    message.reply(
-      "\n" +
-        `${"`"}&job${"`"} : Renvoi tout les metiers du bot.` +
+    message.channel
+      .send(
         "\n" +
-        `${"`"}&job -list${"`"} : Liste des metiers possible.` +
-        "\n" +
-        `${"`"}&job <metier> <lvl>${"`"} : Set le niveau d'un metier pour toi.` +
-        "\n" +
-        `${"`"}&job <@user> <metier> <lvl>${"`"} : Set le niveau d'un metier pour l'user mentionné.` +
-        "\n"
-    );
+          `${"`"}&job${"`"} : Renvoi tout les metiers du bot.` +
+          "\n" +
+          `${"`"}&job -list${"`"} : Liste des metiers possible.` +
+          "\n" +
+          `${"`"}&job -delete <@user>${"`"} : Suprimme les metiers d'un joueurs. (Admin)` +
+          "\n" +
+          `${"`"}&job <metier> <lvl>${"`"} : Set le niveau d'un metier pour toi.` +
+          "\n" +
+          `${"`"}&job <@user> <metier> <lvl>${"`"} : Set le niveau d'un metier pour l'user mentionné. (Admin)` +
+          "\n"
+      )
+      .then((msg) => {
+        setTimeout(() => {
+          msg.delete();
+        }, 10000);
+      });
+    message.delete();
   }
+
   //   console.log(message);
 });
 
